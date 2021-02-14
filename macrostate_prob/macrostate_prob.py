e4f8b5eb5321bd67f407abdeb1f6662e6b2ae0c7
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-02-14 01:26:42 -0500

    updated folders; macrostate

---
+++ macrostate_prob/macrostate_prob.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Statement of question:
+# Two interacting two-state paramagnet, each contains 100 elementary magnetic dipoles,
+# Total number of units of energy relative to the state with all dipoles pointing up is 80.
+# Plot q1 (unit of energy of the first magnet) vs. total macro states. What is the most 
+# probable macro state?
+
+m1 = np.empty(81); m2 = np.empty(81)
+
+for q1 in range(81):
+    q2 = abs(q1 - 80)
+    m1[q1] = np.math.factorial(100)/(np.math.factorial(q1)*np.math.factorial(100 - q1))     #multiplicity function
+    m2[q1] = np.math.factorial(100)/(np.math.factorial(q2)*np.math.factorial(100 - q2))     #multiplicity function
+
+# find the most probable state
+totalM = m1*m2
+print('The most probable macro state is q1 = q2 at position: ' + str(np.where(totalM == totalM.max())))
+
+# plot
+fig, ax = plt.subplots()
+ax.plot(range(81), totalM)
+ax.set_ylabel('Total Macro State')
+ax.set_xlabel('qA')     # = q1
+plt.show()
