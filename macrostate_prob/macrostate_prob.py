6f2bf905f2e4ee6680c99335fa03ed381f44bf1a
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-02-15 00:11:56 -0500

    updated least probable state

--- macrostate_prob/macrostate_prob.py
+++ macrostate_prob/macrostate_prob.py
@@ -14,9 +14,11 @@ for q1 in range(81):
     m1[q1] = np.math.factorial(100)/(np.math.factorial(q1)*np.math.factorial(100 - q1))     #multiplicity function
     m2[q1] = np.math.factorial(100)/(np.math.factorial(q2)*np.math.factorial(100 - q2))     #multiplicity function
 
-# find the most probable state
 totalM = m1*m2
+# find the most probable state
 print('The most probable macro state is q1 = q2 at position: ' + str(np.where(totalM == totalM.max())))
+# find the least probable state
+print('The least probable macro state is at position: ' + str(np.where(totalM == totalM.min())))
 
 # plot
 fig, ax = plt.subplots()
